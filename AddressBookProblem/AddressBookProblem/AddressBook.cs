5dd4f6e36b31cced9456f969ea13b9c4ea9afc35
mohan <sahumohan338@gmail.com>
2022-08-07 00:21:57 -0700

    [mohan]add: Create Address book

---
+++ AddressBookProblem/AddressBookProblem/AddressBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class AddressBook
+    {
+
+List<Contact> cont=new List<Contact> ();
+
+
+        public void Address()
+
+        {
+
+            Contact contact = new Contact();
+            {
+                contact.FirstName = "mohan";
+                contact.LastName = "sahu";
+                contact.Address = "green valey";
+                contact.City = "bhilai";
+                contact.State = "cg";
+                contact.Email = "sahu@12gmail.com";
+                contact.Zip = 12451;
+                contact.Mobile = 744551552;
+
+            }
+            cont.Add(contact);
+        }
+       
+
+            
+        
+        public void Display()
+
+        {
+            //Console.WriteLine(cont.FirstName);            
+
+            foreach (var con in cont)
+            {
+                Console.WriteLine(con.FirstName + " " + con.LastName + " " + con.Address + " " + con.City + " " + con.State + " " + con.Email + " " + con.Zip + " " + con.Mobile);
+            }
+        }
+    }
+}
---
+++ AddressBookProblem/AddressBookProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AddressBookProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Details");
+            AddressBook addressBook = new AddressBook();
+            addressBook.Address();
+            addressBook.Display();
+
+
+
+        }
+    }
+}
