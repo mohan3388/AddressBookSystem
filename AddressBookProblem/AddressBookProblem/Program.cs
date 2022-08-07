570453d87d2d530fbf788f30e0260760bc5fb450
mohan <sahumohan338@gmail.com>
2022-08-07 01:04:21 -0700

    [mohan]add: Edit contact data in addressbook

--- AddressBookProblem/AddressBookProblem/Program.cs
+++ AddressBookProblem/AddressBookProblem/Program.cs
@@ -4,12 +4,36 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter Details");
-            AddressBook addressBook = new AddressBook();
-            addressBook.AddContact();
-            addressBook.Display();
+           
 
+            
+           AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Please Enter the Information");
+            bool check = true;
+            Console.WriteLine("1.Display \n 2.Edit \n");
 
+            while (check)
+            {
+                Console.WriteLine("Enter The Above option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("To Display ");
+                        addressBook.Display();
+                        break;
+                   
+
+                    case 2:
+                        Console.WriteLine("Enter the first name to edit");
+                       
+                        addressBook.Update();
+                        break;
+                    case 0:
+                        check = false; break;
+                }
+
+            }
 
         }
     }
